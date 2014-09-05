569e3930355eb9fcf688fbf47a5bbc1ac76242c0
Happyjessica97 <J.delrio2015@gmail.com>
2014-09-05 11:08:08 -0700

    Awesome Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,46 @@
+int startX = 0; 
+int startY = 150;
+int endX = 0; 
+int endY = 150;
+
+
 void setup()
-{
-  size(300,300);
+{ 
+	size(600,600);
+	strokeWeight(5);
+	background(0,0,40);
+
+
 }
 void draw()
 {
+	int randcolor1 = (int)(Math.random()*401);
+	int randcolor2 = floor(random(1,400));
+	int randcolor3 = floor(random(1,400));
+	int randinteger1 = (int)(Math.random()*11)+1;
+	int randinteger2 = (int)(Math.random()*18)-9;
+	stroke(randcolor1,randcolor2,randcolor3);
+
+	while(endX < 300){
+	endX = startX + randinteger1;
+	println(endX);
+	endY = startY + randinteger2;
+
+	line(startX,startY,endX,endY);
+
+	
+	}startX = endX;
+	startY = endY;
+	
 
 }
 void mousePressed()
-{
+{	
+	startX = 0;
+	startY = 150;
+	endX = 0; 
+	endY = 150;
+
 
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,50 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 0; 
+int startY = 150;
+int endX = 0; 
+int endY = 150;
+
+
 public void setup()
-{
-  size(300,300);
-  noLoop();
+{ 
+	size(600,600);
+	strokeWeight(5);
+	background(0,0,40);
+
+
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	int randcolor1 = (int)(Math.random()*401);
+	int randcolor2 = floor(random(1,400));
+	int randcolor3 = floor(random(1,400));
+	int randinteger1 = (int)(Math.random()*11)+1;
+	int randinteger2 = (int)(Math.random()*18)-9;
+	stroke(randcolor1,randcolor2,randcolor3);
+
+	while(endX < 300){
+	endX = startX + randinteger1;
+	println(endX);
+	endY = startY + randinteger2;
+
+	line(startX,startY,endX,endY);
+
+	
+	}startX = endX;
+	startY = endY;
+	
+
 }
 public void mousePressed()
-{
-  redraw();
+{	
+	startX = 0;
+	startY = 150;
+	endX = 0; 
+	endY = 150;
+
+
 }
 
   static public void main(String[] passedArgs) {
